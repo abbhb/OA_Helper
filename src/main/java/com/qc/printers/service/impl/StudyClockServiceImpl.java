5cb9fe50008edcd5648a8c983a935bf7888f2049
qc <1057117849@qq.com>
2023-07-17 17:35:31 +0800

    学习打卡功能完善

--- src/main/java/com/qc/printers/service/impl/StudyClockServiceImpl.java
+++ src/main/java/com/qc/printers/service/impl/StudyClockServiceImpl.java
@@ -9,10 +9,13 @@ import com.qc.printers.common.MyString;
 import com.qc.printers.common.R;
 import com.qc.printers.config.StudyClockConfig;
 import com.qc.printers.mapper.StudyClockMapper;
+import com.qc.printers.pojo.PageData;
 import com.qc.printers.pojo.StudyClock;
 import com.qc.printers.pojo.User;
 import com.qc.printers.pojo.dto.AddClock30DTO;
 import com.qc.printers.pojo.dto.ClockSelfDTO;
+import com.qc.printers.pojo.vo.ClockSelfEchartsVO;
+import com.qc.printers.pojo.vo.KeepDayDataVO;
 import com.qc.printers.service.IRedisService;
 import com.qc.printers.service.StudyClockService;
 import com.qc.printers.utils.ThreadLocalUtil;
@@ -24,6 +27,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -50,9 +55,20 @@ public class StudyClockServiceImpl extends ServiceImpl<StudyClockMapper, StudyCl
         }
         String value = iRedisService.getValue(MyString.study_clock + currentUser.getId());
         if (StringUtils.isNotEmpty(value)) {
-            throw new CustomException("别想作弊！！！");
+            Long lastTime;
+            Long cuttentTime;
+            try {
+                lastTime = Long.valueOf(value);
+                long l = System.currentTimeMillis();
+                cuttentTime = l;
+            } catch (Exception e) {
+                throw new CustomException("作弊检测！！！");
+            }
+            if (cuttentTime.longValue() - lastTime.longValue() < 27L) {
+                throw new CustomException("别想作弊！！！");
+            }
         }
-        iRedisService.setTokenWithTime(MyString.study_clock + currentUser.getId(), "no", 28L);
+        iRedisService.setTokenWithTime(MyString.study_clock + currentUser.getId(), String.valueOf(System.currentTimeMillis()), 30L);
         LambdaQueryWrapper<StudyClock> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(StudyClock::getUserId, currentUser.getId());
         queryWrapper.eq(StudyClock::getDate, LocalDate.now());
@@ -112,7 +128,10 @@ public class StudyClockServiceImpl extends ServiceImpl<StudyClockMapper, StudyCl
         LocalDateTime firstTime = one.getFirstTime();
         if (firstTime.getHour() > studyClockConfig.getMaxFirstTime()) {
             clockSelfDTO.setIsStandard(false);
-            clockSelfDTO.setWhy(clockSelfDTO.getWhy() + "|| 签到太晚了吧 ||");
+            clockSelfDTO.setWhy(clockSelfDTO.getWhy() + "|| 签到太晚了吧");
+        }
+        if (firstTime.getHour() <= studyClockConfig.getMaxFirstTime() && one.getOldTime() >= studyClockConfig.getMinOldTime()) {
+            clockSelfDTO.setWhy(clockSelfDTO.getWhy() + "今日打卡成功");
         }
         clockSelfDTO.setMinOldTime(studyClockConfig.getMinOldTime());
         //处理日期
@@ -159,5 +178,43 @@ public class StudyClockServiceImpl extends ServiceImpl<StudyClockMapper, StudyCl
         return R.success(clockSelfDTO);
     }
 
+    @Override
+    public R<ClockSelfEchartsVO> getSelfClockEcharts() {
+        User currentUser = ThreadLocalUtil.getCurrentUser();
+        if (currentUser == null) {
+            throw new CustomException("登录失效", Code.DEL_TOKEN);
+        }
+        //查找当前用户当前月的信息
+        List<StudyClock> studyClock = studyClockMapper.getStudyClock(currentUser.getId(), LocalDateTime.now().withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0), LocalDateTime.now());
+        int currentDays = 0;
+        List<String> xTextList = new ArrayList<>();
+        List<Double> xValueList = new ArrayList<>();
+        // 有7天取7天，没有就全取
+        for (StudyClock s :
+                studyClock) {
+            xValueList.add(s.getOldTime());
+            xTextList.add(s.getDate().toString());
+            currentDays += 1;
+            if (currentDays == 7) {
+                break;
+            }
+        }
+        Collections.reverse(xTextList);
+        Collections.reverse(xValueList);
+        ClockSelfEchartsVO clockSelfEchartsVO = new ClockSelfEchartsVO();
+        clockSelfEchartsVO.setXTextList(xTextList);
+        clockSelfEchartsVO.setXValueList(xValueList);
+        return R.success(clockSelfEchartsVO);
+
+    }
+
+    @Override
+    public R<PageData<List<KeepDayDataVO>>> getAdminDayData(Integer pageNum, Integer pageSize, String name, String date, String groupId) {
+        return null;
+    }
+    /**
+     * 获取admin管理面板日数据
+     * @return
+     */
 
 }
