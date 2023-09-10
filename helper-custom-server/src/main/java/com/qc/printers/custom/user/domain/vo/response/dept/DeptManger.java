77ba056658cbc8c5090c5c8bde8d00a589266c3b
qc2003 <qc2003020403@163.com>
2023-09-10 16:53:44 +0800

    部门关系

--- helper-custom-server/src/main/java/com/qc/printers/custom/user/domain/vo/response/dept/DeptManger.java
+++ helper-custom-server/src/main/java/com/qc/printers/custom/user/domain/vo/response/dept/DeptManger.java
@@ -1,6 +1,7 @@
 package com.qc.printers.custom.user.domain.vo.response.dept;
 
 import com.qc.printers.common.user.domain.entity.SysDept;
+import com.qc.printers.common.user.domain.entity.SysRole;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -12,6 +13,9 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class DeptManger extends SysDept implements Serializable {
+
+    private List<SysRole> roles;
+
     private List<DeptManger> children;
 
 }
--- helper-custom-server/src/main/java/com/qc/printers/custom/user/service/impl/DeptServiceImpl.java
+++ helper-custom-server/src/main/java/com/qc/printers/custom/user/service/impl/DeptServiceImpl.java
@@ -1,9 +1,16 @@
 package com.qc.printers.custom.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.qc.printers.common.common.CustomException;
 import com.qc.printers.common.user.domain.entity.SysDept;
+import com.qc.printers.common.user.domain.entity.SysRole;
+import com.qc.printers.common.user.domain.entity.SysRoleDept;
+import com.qc.printers.common.user.domain.entity.User;
 import com.qc.printers.common.user.service.ISysDeptService;
+import com.qc.printers.common.user.service.ISysRoleDeptService;
+import com.qc.printers.common.user.service.ISysRoleService;
+import com.qc.printers.common.user.service.IUserService;
 import com.qc.printers.custom.user.domain.vo.response.dept.DeptManger;
 import com.qc.printers.custom.user.service.DeptService;
 import com.qc.printers.custom.user.utils.DeptMangerHierarchyBuilder;
@@ -14,7 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -23,10 +32,20 @@ public class DeptServiceImpl implements DeptService {
     @Autowired
     private ISysDeptService iSysDeptService;
 
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private ISysRoleDeptService iSysRoleDeptService;
+
+    @Autowired
+    private ISysRoleService iSysRoleService;
 
     @Override
     public List<DeptManger> getDeptList() {
         List<SysDept> list = iSysDeptService.list();
+        Set<SysRoleDept> sysRoleDepts = new HashSet<>(iSysRoleDeptService.list());
+        Set<SysRole> sysRoles = new HashSet<>(iSysRoleService.list());
         list.sort((m1, m2) -> {
             Long order1 = m1.getId();
             Long order2 = m2.getId();
@@ -35,10 +54,10 @@ public class DeptServiceImpl implements DeptService {
         if (list.size() < 1) {
             throw new CustomException("系统异常，请添加根节点(id:0)");
         }
-
-        DeptMangerHierarchyBuilder deptMangerHierarchyBuilder = new DeptMangerHierarchyBuilder(list);
+        DeptMangerHierarchyBuilder deptMangerHierarchyBuilder = new DeptMangerHierarchyBuilder(list, sysRoles, sysRoleDepts);
         List<DeptManger> deptMangers = deptMangerHierarchyBuilder.buildHierarchy();
         sortRecursion(deptMangers);
+
         return deptMangers;
     }
 
@@ -62,6 +81,7 @@ public class DeptServiceImpl implements DeptService {
     @Transactional
     @Override
     public String addDept(DeptManger deptManger) {
+        //todo: 角色部门关系
         if (StringUtils.isEmpty(deptManger.getDeptName())) {
             throw new CustomException("请输入DeptName");
         }
@@ -74,13 +94,42 @@ public class DeptServiceImpl implements DeptService {
         SysDept sysDept = new DeptManger();
         BeanUtils.copyProperties(deptManger, sysDept);
         sysDept.setId(null);
+        sysDept.setAncestors(getAncestrs(deptManger.getParentId()));
         iSysDeptService.save(sysDept);
+        // 完善部门角色关系表
+        if (deptManger.getRoles() != null && deptManger.getRoles().size() > 0) {
+            for (SysRole sysRole :
+                    deptManger.getRoles()) {
+                SysRoleDept sysRoleDept = new SysRoleDept();
+                sysRoleDept.setDeptId(deptManger.getId());
+                sysRoleDept.setRoleId(sysRole.getId());
+                iSysRoleDeptService.save(sysRoleDept);
+            }
+        }
         return "添加成功";
+    }
+
+    @Transactional
+    public String getAncestrs(Long thisParentId) {
+        if (thisParentId.equals(0L)) {
+            return null;
+        }
+        String s = String.valueOf(thisParentId);
+        SysDept sysDept = iSysDeptService.getById(thisParentId);
+        ;
+        while (sysDept != null && sysDept.getParentId() != null && sysDept.getParentId().equals(1L)) {
+            s = String.valueOf(sysDept.getId()) + "," + s;
+            sysDept = iSysDeptService.getById(sysDept.getParentId());
+        }
+        s = "1" + "," + s;
+        //最终最前面加上1
+        return s;
     }
 
     @Transactional
     @Override
     public String updateDept(DeptManger deptManger) {
+        //todo: 角色部门关系
         if (deptManger.getId() == null) {
             throw new CustomException("无操作对象");
         }
@@ -93,6 +142,13 @@ public class DeptServiceImpl implements DeptService {
         if (StringUtils.isEmpty(deptManger.getStatus())) {
             deptManger.setStatus("1");
         }
+        if (deptManger.getId().equals(1L)) {
+            // 系统部门，禁止停用
+            if (!deptManger.getStatus().equals("1")) {
+                throw new CustomException("系统部门禁止停用");
+            }
+            deptManger.setParentId(0L);
+        }
         LambdaUpdateWrapper<SysDept> lambdaUpdateWrapper = new LambdaUpdateWrapper<>();
         lambdaUpdateWrapper.eq(SysDept::getId, deptManger.getId());
         lambdaUpdateWrapper.set(SysDept::getDeptName, deptManger.getDeptName());
@@ -104,16 +160,55 @@ public class DeptServiceImpl implements DeptService {
         lambdaUpdateWrapper.set(SysDept::getOrderNum, deptManger.getOrderNum());
         lambdaUpdateWrapper.set(SysDept::getStatus, deptManger.getStatus());
         lambdaUpdateWrapper.set(SysDept::getPhone, deptManger.getPhone());
+        lambdaUpdateWrapper.set(SysDept::getAncestors, getAncestrs(deptManger.getParentId()));
         iSysDeptService.update(lambdaUpdateWrapper);
+        //还得更新角色关联表 way:removeReBuild
+        LambdaQueryWrapper<SysRoleDept> sysRoleDeptLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        sysRoleDeptLambdaQueryWrapper.eq(SysRoleDept::getDeptId, deptManger.getId());
+        iSysRoleDeptService.remove(sysRoleDeptLambdaQueryWrapper);
+        //ReBuildNewRoleDept
+        if (deptManger.getRoles() != null && deptManger.getRoles().size() > 0) {
+            for (SysRole sysRole :
+                    deptManger.getRoles()) {
+                SysRoleDept sysRoleDept = new SysRoleDept();
+                sysRoleDept.setDeptId(deptManger.getId());
+                sysRoleDept.setRoleId(sysRole.getId());
+                iSysRoleDeptService.save(sysRoleDept);
+            }
+        }
+
+
         return "更新成功";
     }
 
     @Transactional
     @Override
     public String deleteDept(String id) {
+        if (id.equals("1")) {
+            throw new CustomException("系统部门禁止删除");
+        }
         //需要确保该部门以及下属部门没有绑定用户
-        //符合条件进行级联 删除(和菜单逻辑类似)
-        return null;
+        LambdaQueryWrapper<SysDept> sysDeptLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        sysDeptLambdaQueryWrapper.eq(SysDept::getId, Long.valueOf(id)).or().like(SysDept::getAncestors, Long.valueOf(id));
+        // list 就是要确认的，确认到哪个部门有用户
+        // 都没有用户就全删除
+        List<SysDept> list = iSysDeptService.list(sysDeptLambdaQueryWrapper);
+        for (SysDept sysDept : list) {
+            LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            userLambdaQueryWrapper.eq(User::getDeptId, sysDept.getId());
+            int count = iUserService.count(userLambdaQueryWrapper);
+            if (count != 0) {
+                throw new CustomException("[" + sysDept.getDeptName() + "]部门下存在用户！禁止直接删除");
+            }
+        }
+        //符合条件进行级联 删除(和菜单逻辑类似) 一并删除部门角色关联
+        for (SysDept sysDept : list) {
+            LambdaQueryWrapper<SysRoleDept> sysRoleDeptLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            sysRoleDeptLambdaQueryWrapper.eq(SysRoleDept::getDeptId, sysDept.getId());
+            iSysRoleDeptService.remove(sysRoleDeptLambdaQueryWrapper);
+            iSysDeptService.removeById(sysDept.getId());
+        }
+        return "删除成功";
     }
 
 
