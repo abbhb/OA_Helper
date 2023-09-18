037a74d7a5a965c430849a220fc5268a30b745e0
qc2003 <qc2003020403@163.com>
2023-09-18 19:18:27 +0800

    chat功能--暂未报错

---
+++ helper-tools/helper-transaction/src/main/java/com/qc/helper/transaction/mapper/SecureInvokeRecordMapper.java
@@ -0,0 +1,8 @@
+package com.qc.helper.transaction.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qc.helper.transaction.domain.entity.SecureInvokeRecord;
+
+public interface SecureInvokeRecordMapper extends BaseMapper<SecureInvokeRecord> {
+
+}
