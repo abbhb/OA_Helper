22550f38d53b0a0d9806bc63b317d1834976f890
Cheng Qiu <qc2003020403@163.com>
2023-11-18 15:20:33 +0800

    一键打印

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM openjdk:17-jdk-alpine
 LABEL authors="qc200"
 
 WORKDIR /app
+RUN apk --update add tzdata && \
+    cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone && \
+    apk del tzdata && \
+    rm -rf /var/cache/apk/* \
+
+
 COPY helper-custom-server/target/helper-custom-server-1.0-SNAPSHOT.jar /app
 
 CMD ["java", "-jar", "helper-custom-server-1.0-SNAPSHOT.jar"]
--- helper-common/src/main/java/com/qc/printers/common/common/utils/RandomName.java
+++ helper-common/src/main/java/com/qc/printers/common/common/utils/RandomName.java
@@ -1,12 +1,14 @@
 package com.qc.printers.common.common.utils;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class RandomName {
-    public static String getRandomName(String fileName){
-        int index=fileName.lastIndexOf(".");
-        String houzhui=fileName.substring(index);//获取后缀名
-        String uuidFileName= UUID.randomUUID().toString().replace("-","")+houzhui;
+    public static String getRandomName(String fileName) {
+        int index = fileName.lastIndexOf(".");
+        String houzhui = fileName.substring(index);//获取后缀名
+        String fileNameWithoutSuffix = fileName.substring(0, index - 1);
+        String uuidFileName = fileNameWithoutSuffix + Instant.now().toEpochMilli() + houzhui;
         return uuidFileName;
     }
 
