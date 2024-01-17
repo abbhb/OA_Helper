6dd739f9adebfd4547e6a2b1a6c6f43f5165f123
Cheng Qiu <qc2003020403@163.com>
2024-01-17 17:29:26 +0800

    v3.0.3 --fix:oauth2-server state不是必须字段

--- helper-custom-server/src/main/java/com/qc/printers/custom/navigation/service/impl/QuickNavigationServiceImpl.java
+++ helper-custom-server/src/main/java/com/qc/printers/custom/navigation/service/impl/QuickNavigationServiceImpl.java
@@ -1,11 +1,14 @@
 package com.qc.printers.custom.navigation.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.qc.printers.common.common.CustomException;
 import com.qc.printers.common.common.R;
 import com.qc.printers.common.common.utils.ThreadLocalUtil;
 import com.qc.printers.common.common.utils.permissionstringsplit.MySplit;
+import com.qc.printers.common.navigation.dao.QuickNavigationDeptDao;
 import com.qc.printers.common.navigation.domain.entity.QuickNavigationCategorize;
+import com.qc.printers.common.navigation.domain.entity.QuickNavigationDept;
 import com.qc.printers.common.navigation.domain.entity.QuickNavigationItem;
 import com.qc.printers.common.navigation.service.IQuickNavigationCategorizeService;
 import com.qc.printers.common.navigation.service.IQuickNavigationItemService;
@@ -31,15 +34,46 @@ public class QuickNavigationServiceImpl implements QuickNavigationService {
     @Autowired
     private IQuickNavigationItemService iQuickNavigationCategorizeItemService;
 
+    @Autowired
+    private QuickNavigationDeptDao quickNavigationDeptDao;
 
+    /**
+     * 只返回有权限看见的
+     *
+     * @return
+     */
     @Override
     public R<List<QuickNavigationResult>> list() {
         UserInfo currentUser = ThreadLocalUtil.getCurrentUser();
         if (currentUser == null) {
             return R.error("异常1");
         }
-        List<QuickNavigationCategorize> quickNavigationCategorizes = iQuickNavigationCategorizeService.list();
-        List<QuickNavigationItem> quickNavigationItems = iQuickNavigationCategorizeItemService.list();
+        // 当前用户所属部门id
+        Long currentUserDeptId = currentUser.getDeptId();
+        //todo：先查出可见性为0的部门，再找为1的，而且筛选出当前部门id在允许范围的，综合为最终返回结果
+        LambdaQueryWrapper<QuickNavigationDept> quickNavigationDeptLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        quickNavigationDeptLambdaQueryWrapper.eq(QuickNavigationDept::getDeptId, currentUserDeptId);
+        quickNavigationDeptLambdaQueryWrapper.select(QuickNavigationDept::getQuickNavCategorizeId);
+        // 找到该部门被授权的分类id列表
+        List<Long> quickNavigationDeptList = quickNavigationDeptDao.list(quickNavigationDeptLambdaQueryWrapper).stream().map(QuickNavigationDept::getQuickNavCategorizeId).toList();
+        LambdaQueryWrapper<QuickNavigationCategorize> quickNavigationCategorizeLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        quickNavigationCategorizeLambdaQueryWrapper.eq(QuickNavigationCategorize::getVisibility, 0);
+        if (quickNavigationDeptList != null && quickNavigationDeptList.size() > 0) {
+            quickNavigationCategorizeLambdaQueryWrapper.or().in(QuickNavigationCategorize::getId, quickNavigationDeptList);
+        }
+        // 找到该部门被授权的分类id列表对应的分类实体类列表
+        List<QuickNavigationCategorize> quickNavigationCategorizes = iQuickNavigationCategorizeService.list(quickNavigationCategorizeLambdaQueryWrapper);
+        List<Long> categorizeIds = quickNavigationCategorizes.stream().map(QuickNavigationCategorize::getId).toList();
+        LambdaQueryWrapper<QuickNavigationItem> quickNavigationItemLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        if (categorizeIds == null) {
+            throw new CustomException("为空");
+        }
+        if (categorizeIds.size() == 0) {
+            throw new CustomException("为空");
+        }
+        quickNavigationItemLambdaQueryWrapper.in(QuickNavigationItem::getCategorizeId, categorizeIds);
+        // 找到这些分类实体类列表id所对应的item实体类列表
+        List<QuickNavigationItem> quickNavigationItems = iQuickNavigationCategorizeItemService.list(quickNavigationItemLambdaQueryWrapper);
         List<QuickNavigationResult> quickNavigationResults = new ArrayList<>();
 
 
