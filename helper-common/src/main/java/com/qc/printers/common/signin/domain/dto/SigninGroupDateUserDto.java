503431f77994801d3216022789c39654c931162d
abbhb <qc2003020403@163.com>
2024-05-26 14:02:51 +0800

    v3.5.6.11 @add:数据统计功能，大屏接口

--- helper-common/src/main/java/com/qc/printers/common/signin/domain/dto/SigninGroupDateUserDto.java
+++ helper-common/src/main/java/com/qc/printers/common/signin/domain/dto/SigninGroupDateUserDto.java
@@ -35,6 +35,12 @@ public class SigninGroupDateUserDto implements Serializable {
     private Integer bcCount;
 
     /**
+     * 昨天概况状态，缺勤或者正常，异常为存在迟到或者早退
+     * 根据所有班次的状态综合，全出勤为正常，迟到和早退存在但不存在缺勤就是异常，全正常为正常
+     */
+    private Integer state;
+
+    /**
      * 该用户详细的logList
      */
     private List<SigninLogCliBcDto> logList;
--- helper-common/src/main/java/com/qc/printers/common/signin/domain/dto/SigninLogRealYiQianDaoDto.java
+++ helper-common/src/main/java/com/qc/printers/common/signin/domain/dto/SigninLogRealYiQianDaoDto.java
@@ -1,6 +1,11 @@
 package com.qc.printers.common.signin.domain.dto;
 
-public class SigninLogRealYiQianDaoDto {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SigninLogRealYiQianDaoDto implements Serializable {
     private String tag;// 请假就写请假，迟到早退都直接标注，最近班次始终只有单班！一个班次里上班下班是分开的
 
     private String name;
