97df9bc828acdbc158b6cb8b84c383e0f630f5f8
Cheng Qiu <qc2003020403@163.com>
2024-06-03 15:11:48 +0800

    v3.5.6.13 @fix:修复取消打印的通知错误 @fix:修复打印页数统计异常 @fix:同步人脸识别代码

---
+++ helper-common/src/main/java/com/qc/printers/common/signin/domain/resp/AddLogExtInfo.java
@@ -0,0 +1,19 @@
+package com.qc.printers.common.signin.domain.resp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AddLogExtInfo implements Serializable {
+
+    private String avatarUrl;
+
+    /**
+     * 完整部门名称-分割
+     */
+    private String deptName;
+
+
+}
